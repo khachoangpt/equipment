fd56f8b493764729e03750de5be15187b9d648ee
Hoang Nguyen <hoangncd3@gmail.com>
2025-05-31 10:33:57 +0700

    fix: logout

--- src/hooks/use-logout.tsx
+++ src/hooks/use-logout.tsx
@@ -1,14 +1,10 @@
-import { pageList } from '@/configs/routes'
 import { COOKIES } from '@/constants'
 import { deleteCookie } from 'cookies-next'
-import { useRouter } from 'next/navigation'
 
 const useLogout = () => {
-	const router = useRouter()
-
 	const logout = async () => {
 		await deleteCookie(COOKIES.JWT)
-		router.push(pageList.home.href)
+		window.location.reload()
 		return
 	}
 
