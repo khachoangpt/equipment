2ba0619354216c1f4dfb0dfeadb8bd67b07946fa
layer-quynh <quynhkun15101998@gmail.com>
2026-02-09 00:24:13 +0700

    fix: remove console.log

--- src/modules/equipment-set-inventory/components/organisms/UpdateInventoryDialog.tsx
+++ src/modules/equipment-set-inventory/components/organisms/UpdateInventoryDialog.tsx
@@ -86,9 +86,7 @@ const UpdateInventoryDialog = ({
 
 	const onSubmit = async (formData: UpdateInventoryForm) => {
 		// Nếu currentStatus là "Mới" thì đổi thành "Trong kho" trước khi call API
-		console.log('currentStatus', currentStatus);
 		const statusToSend = currentStatus === 'Mới' ? 'Trong kho' : currentStatus
-		console.log('statusToSend', statusToSend);
 
 		updateInventory(
 			{
